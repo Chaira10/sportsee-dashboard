af5fddd38080ec7923544c3f0d9a33bd4f181a8d
Chaira <chaira10@hotmail.fr>
2023-09-25 02:00:11 +0200

    add navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,18 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav>Navbar
-    
+    <nav>
+    <div className="img-container">
+        <img src="/13.png" alt="logo" className="nav-logo"/>
+    </div>
+    <div className="link-container">
+        <ul className="navbar">
+            <li className="nav-link">Accueil</li>
+            <li className="nav-link">Profil</li>
+            <li className="nav-link">Réglage</li>
+            <li className="nav-link">Communauté</li>
+        </ul>
+    </div>
     
     </nav>
   )
